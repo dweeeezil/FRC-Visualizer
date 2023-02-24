0433d367bc24e56e3a3915dd1af5b426e95b9cc3
Alex Kelly <109198397+dweeeezil@users.noreply.github.com>
2023-02-23 17:11:48 -0800

    Add files via upload

--- src/main/java/com/RobotTracker/Server/DataController.java
+++ src/main/java/com/RobotTracker/Server/DataController.java
@@ -1,50 +1,59 @@
 package com.RobotTracker.Server;
 
+
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+
+
 @RestController
 public class DataController 
 {
+    NetworkTable Table;
+    
+
+    public DataController()
+    {
+        NetworkTable.setClientMode();
+        NetworkTable.setIPAddress("10.37.49.222");
+        Table = NetworkTable.getTable("datatable");
+    }
+    
+    
     @GetMapping(value = "GetData/", produces = { "application/json", "text/html" })
     public ResponseEntity <String> GetData(@RequestParam(value = "SET", 
                                                                  defaultValue = "") 
                                                                  final String Set) 
-    {     
+    {    
+        double TX = Table.getNumber("X", 0.0);
+        double TY = Table.getNumber("Y", 0.0);
+
+        System.out.println("X: " + TX + "    Y: " + TY); 
+        
         JSONObject BaseResults = new JSONObject();
 
+        Double Radius = 100.0;
+        Integer Center = 200;
+
+        Radius = Radius + 50.0 + 50.0 * Math.sin(System.currentTimeMillis() / 100.0);
+
+        Double XVal = Radius * Math.cos(System.currentTimeMillis() / 1000.0);
+        Double YVal = Radius * Math.sin(System.currentTimeMillis() / 1000.0);
         
-        /*
-
-        Comment out a bunch of the stuff below and replace it with camera & encoder data.
-
-        I don't know how to pull data from the RoboRio or the Beelink, so this will need to be replaced with actual data.
-
-        */
-
-
-        Double Radius = 100.0;  //This isn't needed
-        Integer Center = 200;   //This is needed, use this to set the world/coordinate origin.
-
-        Radius = Radius + 50.0 + 50.0 * Math.sin(System.currentTimeMillis() / 100.0);   //This isn't needed
-
-        Double XVal = Radius * Math.cos(System.currentTimeMillis() / 1000.0);   //This is needed, but the sin & cos stuff should be replaced with location data.
-        Double YVal = Radius * Math.sin(System.currentTimeMillis() / 1000.0);   //^^^^^^^^
+        Integer X = Center + XVal.intValue();
+        Integer Y = Center + YVal.intValue();
         
-        Integer X = Center + XVal.intValue();   //This is probably needed, but XVal and YVal must be changed.
-        Integer Y = Center + YVal.intValue();   //^^^^^^^^^
-        
-        BaseResults.put("camx", X); //Displays camera location x
-        BaseResults.put("camy", Y); //Displays camera location y
+        BaseResults.put("camx", X);
+        BaseResults.put("camy", Y);
 
-
-        //This controls the encoder visualization, generally the same rules apply as to the camera data.
         Double ErrorRadius = Radius + 10.0 * Math.sin(System.currentTimeMillis() / 20.0);
-        XVal = ErrorRadius * Math.cos(System.currentTimeMillis() / 1000.0); //Rplace with real data
-        YVal = ErrorRadius * Math.sin(System.currentTimeMillis() / 1000.0); //Rplace with real data
+        XVal = ErrorRadius * Math.cos(System.currentTimeMillis() / 1000.0);
+        YVal = ErrorRadius * Math.sin(System.currentTimeMillis() / 1000.0);
         
         X = Center + XVal.intValue();
         Y = Center + YVal.intValue();
